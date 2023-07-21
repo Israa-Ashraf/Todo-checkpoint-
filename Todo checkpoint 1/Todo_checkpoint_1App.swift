1f019a2e79e7e51807545c0353d80ac6c65bb91f
Israa <israaashraf1@icloud.com>
2023-07-21 20:10:29 +0400

    Initial Commit

---
+++ Todo checkpoint 1/Todo_checkpoint_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_checkpoint_1App.swift
+//  Todo checkpoint 1
+//
+//  Created by Israa Ashraf on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_checkpoint_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
