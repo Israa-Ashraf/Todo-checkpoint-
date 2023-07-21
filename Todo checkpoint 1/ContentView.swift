881643ecc5f5ab85194f906174e7db0595a2e345
Israa <israaashraf1@icloud.com>
2023-07-21 21:02:42 +0400

    Done

--- Todo checkpoint 1/ContentView.swift
+++ Todo checkpoint 1/ContentView.swift
@@ -1,31 +1,36 @@
-//
-//  ContentView.swift
-//  Todo checkpoint 1
-//
-//  Created by Israa Ashraf on 21/07/2023.
-//
-
 import SwiftUI
+
 struct ContentView: View {
-    
-    @State private var todos = [Todo(title: "Feed the cat"),
-                        Todo(title: "Play with cat"),
+    @State private var todos = [Todo(title: "Feed the cat", isCompleted: true),
+                        Todo(title: "Play with cat", subtitle: "Use his favourite String!"),
                         Todo(title: "Get allergies"),
                         Todo(title: "Run away from cat"),
                         Todo(title: "Get a new cat")]
-    
+
     var body: some View {
+
         NavigationStack {
-            List(todos) { todo in
-                Text(todo.title)
+            List($todos) { $todo in
+                HStack {
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        .onTapGesture {
+                            todo.isCompleted.toggle()
+                        }
+                    VStack(alignment: .leading) {
+                        Text(todo.title)
+                            .strikethrough(todo.isCompleted)
+                   }
+                }
             }
             .navigationTitle("Todos")
         }
+                
     }
 }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
 
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Todo checkpoint 1/Todo.swift
+++ Todo checkpoint 1/Todo.swift
@@ -1,14 +1,10 @@
-//
-//  Todo.swift
-//  Todo checkpoint 1
-//
-//  Created by Israa Ashraf on 21/07/2023.
-//
-
 import Foundation
+
 struct Todo: Identifiable {
+    
     let id = UUID()
     var title: String
-    var isCompleted: Bool = false
+    var subtitle = ""
+    var isCompleted = false
+    
 }
-
